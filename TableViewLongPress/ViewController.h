176b51007bb41c04745413a818316b09196c081f
NatureZhang <895234387@qq.com>
2016-02-18 14:09:12 +0800

    Initial Commit

---
+++ TableViewLongPress/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableViewLongPress
+//
+//  Created by zhangdong on 16/2/18.
+//  Copyright © 2016年 zhangdong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TableViewLongPress/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TableViewLongPress
+//
+//  Created by zhangdong on 16/2/18.
+//  Copyright © 2016年 zhangdong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
